280de18d8536e107dd49c144cb03be271edde49b
Johannes Normann Jensen <johannes@nestech.dk>
2021-05-28 00:35:16 +0200

    Add slider for fun

--- libs/blog/shell/src/lib/blog-shell.module.ts
+++ libs/blog/shell/src/lib/blog-shell.module.ts
@@ -1,11 +1,17 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { MatSliderModule } from '@angular/material/slider';
 import { EmptyComponent } from './component/empty/empty.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatSliderModule,
+  ],
   declarations: [
     EmptyComponent
   ],
+  exports: [
+  ],
 })
-export class BlogShellModule {}
+export class BlogShellModule { }
--- libs/blog/shell/src/lib/component/empty/empty.component.stories.ts
+++ libs/blog/shell/src/lib/component/empty/empty.component.stories.ts
@@ -1,17 +1,25 @@
 
-import { Story } from '@storybook/angular';
+import { CommonModule } from '@angular/common';
+import { MatSliderModule } from '@angular/material/slider';
+import { moduleMetadata, Story } from '@storybook/angular';
 import { EmptyComponent } from './empty.component';
 
 export default {
   title: 'Shell/EmptyComponent',
   component: EmptyComponent,
+  decorators: [
+    moduleMetadata({
+      //👇 Imports both components to allow component composition with Storybook
+      imports: [CommonModule, MatSliderModule,],
+    }),
+  ],
   argTypes: {
     title: {
       options: ['primary', 'secondary'],
       control: { type: 'radio' }
     }
   }
-}
+};
 
 const EmptyStory: Story<EmptyComponent> = (args) => ({
   props: args,
